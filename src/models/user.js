a04b580002ffa3071dfe01d35c3929b08dcac527
Hunter Raffety <hsraffety@gmail.com>
2019-11-17 20:01:02 -0700

    make it better

--- src/models/user.js
+++ src/models/user.js
@@ -1,41 +1,41 @@
-import db from "../../data/dbConfig";
+import db from '../../data/dbConfig'
 
 export async function findUserByLogin(email, password) {
-  let res = await db("user").where({ email, password });
-  console.log(res);
+  let res = await db('user').where({ email, password })
+  console.log(res)
   if (res == false) {
-    res = false;
+    res = false
   }
-  return res;
+  return res
 }
 
 export function findUserByEmail(email) {
-  return db("user").where({ email });
+  return db('user').where({ email })
 }
 
 export function findUser(id) {
-  return db("user")
+  return db('user')
     .first()
-    .where({ id });
+    .where({ id })
 }
 
 export function findUsers() {
-  return db("users");
+  return db('users')
 }
 
 export function createUser(user) {
-  return db("user").insert(user);
+  return db('user').insert(user)
 }
 
 export async function updateUser(id, updatedUser) {
-  await db("user")
+  await db('user')
     .where({ id })
-    .update(updatedUser);
-  return db("user").where({ id });
+    .update(updatedUser)
+  return db('user').where({ id })
 }
 
 export function deleteUser(id) {
-  return db("user")
+  return db('user')
     .where({ id })
-    .del();
+    .del()
 }
