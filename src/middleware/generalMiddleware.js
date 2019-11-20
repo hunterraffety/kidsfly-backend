977c7df54e1bcc8a41542ef82589abe98e5f6dd0
Hunter Raffety <hsraffety@gmail.com>
2019-11-19 18:05:23 -0700

    updating cors

--- src/middleware/generalMiddleware.js
+++ src/middleware/generalMiddleware.js
@@ -1,14 +1,9 @@
-import express from "express";
-import cors from "cors";
+import express from 'express'
+import cors from 'cors'
 
 const setGeneralMiddleware = server => {
-  server.use(express.json());
-  server.use(
-    cors({
-      credentials: true,
-      origin: true
-    })
-  );
-};
+  server.use(express.json())
+  server.use(cors())
+}
 
-export default setGeneralMiddleware;
+export default setGeneralMiddleware
